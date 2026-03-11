548fea9ae7375a63ff5c468006916f5958365c1e
Vu Kim An <anvu-neli@Vus-MacBook-Pro-2.local>
2026-03-11 15:22:32 +0700

    feat: Restructure  to a dedicated directory with enhanced configuration and add Postgres and Redis services to Docker Compose.

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+// import { ServeStaticModule } from '@nestjs/serve-static';
+// import { join } from 'path';
+// import { AuthModule } from 'src/api/auth/auth.module';
+// import { config } from 'config/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+// import { DatabaseModule } from '@app/database';
+// import { UserModule } from 'src/api/user/user.module';
+import { CryptoModule } from '@app/crypto';
+import { JwtModule } from '@app/jwt';
+import configuration from '@app/config';
+// import { RedisModule } from '@app/redis';
+// import { providers } from './app.provider';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`.env.${process.env.NODE_ENV}`, '.env'],
+      load: [configuration],
+      expandVariables: true,
+    }),
+    // ServeStaticModule.forRoot({
+    //   rootPath: join(process.cwd(), 'client'),
+    // }),
+    // DatabaseModule,
+    JwtModule,
+    CryptoModule,
+    // AuthModule,
+    // UserModule,
+    // RedisModule,
+  ],
+  controllers: [AppController],
+  // providers: [AppService, ...providers],
+  providers: [AppService],
+})
+export class AppModule {}
